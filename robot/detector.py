0d95317e807dba3d39253419fd91e84e4ab53eae
man.shen <man.shen@tmirob.com>
2025-01-03 18:54:05 +0800

    revert

--- robot/detector.py
+++ robot/detector.py
@@ -1,8 +1,6 @@
 import time
 import os
 import threading
-import pvporcupine
-from pvrecorder import PvRecorder
 
 from snowboy import snowboydecoder
 from robot import config, logging, utils, constants
@@ -14,58 +12,6 @@ recorder = None
 porcupine = None
 
 
-def detect_porcupine(wukong, access_key, keyword_paths, keywords):
-    try:
-        while True:
-            pcm = recorder.read()
-
-            result = porcupine.process(pcm)
-            if result >= 0:
-                kw = keyword_paths[result] if keyword_paths else keywords[result]
-                logger.info(
-                    "[porcupine] Keyword {} Detected at time {}".format(
-                        kw,
-                        time.strftime(
-                            "%Y-%m-%d %H:%M:%S", time.localtime(time.time())
-                        ),
-                    )
-                )
-                wukong._detected_callback(False)
-                recorder.stop()
-                wukong.conversation.interrupt()
-                query = wukong.conversation.activeListen()
-                wukong.conversation.doResponse(query)
-                recorder.start()
-    except pvporcupine.PorcupineActivationError as e:
-        logger.error("[Porcupine] AccessKey activation error", stack_info=True)
-        raise e
-    except pvporcupine.PorcupineActivationLimitError as e:
-        logger.error(
-            f"[Porcupine] AccessKey {access_key} has reached it's temporary device limit",
-            stack_info=True,
-        )
-        raise e
-    except pvporcupine.PorcupineActivationRefusedError as e:
-        logger.error(
-            "[Porcupine] AccessKey '%s' refused" % access_key, stack_info=True
-        )
-        raise e
-    except pvporcupine.PorcupineActivationThrottledError as e:
-        logger.error(
-            "[Porcupine] AccessKey '%s' has been throttled" % access_key,
-            stack_info=True,
-        )
-        raise e
-    except pvporcupine.PorcupineError as e:
-        logger.error("[Porcupine] 初始化 Porcupine 失败", stack_info=True)
-        raise e
-    except KeyboardInterrupt:
-        logger.info("Stopping ...")
-    finally:
-        porcupine and porcupine.delete()
-        recorder and recorder.delete()
-
-
 def initDetector(wukong):
     """
     初始化离线唤醒热词监听器，支持 snowboy 和 porcupine 两大引擎
@@ -73,6 +19,9 @@ def initDetector(wukong):
     global porcupine, recorder, detector
     if config.get("detector", "snowboy") == "porcupine":
         logger.info("使用 porcupine 进行离线唤醒")
+
+        import pvporcupine
+        from pvrecorder import PvRecorder
 
         access_key = config.get("/porcupine/access_key")
         keyword_paths = config.get("/porcupine/keyword_paths")
@@ -97,9 +46,55 @@ def initDetector(wukong):
         logger.info("读取buffer：" + str(porcupine.frame_length))
         recorder.start()
 
-        threading.Thread(
-            target=detect_porcupine, args=(wukong, access_key, keyword_paths, keywords), daemon=True
-        ).start()
+        try:
+            while True:
+                pcm = recorder.read()
+
+                result = porcupine.process(pcm)
+                if result >= 0:
+                    kw = keyword_paths[result] if keyword_paths else keywords[result]
+                    logger.info(
+                        "[porcupine] Keyword {} Detected at time {}".format(
+                            kw,
+                            time.strftime(
+                                "%Y-%m-%d %H:%M:%S", time.localtime(time.time())
+                            ),
+                        )
+                    )
+                    wukong._detected_callback(False)
+                    recorder.stop()
+                    wukong.conversation.interrupt()
+                    query = wukong.conversation.activeListen()
+                    wukong.conversation.doResponse(query)
+                    recorder.start()
+        except pvporcupine.PorcupineActivationError as e:
+            logger.error("[Porcupine] AccessKey activation error", stack_info=True)
+            raise e
+        except pvporcupine.PorcupineActivationLimitError as e:
+            logger.error(
+                f"[Porcupine] AccessKey {access_key} has reached it's temporary device limit",
+                stack_info=True,
+            )
+            raise e
+        except pvporcupine.PorcupineActivationRefusedError as e:
+            logger.error(
+                "[Porcupine] AccessKey '%s' refused" % access_key, stack_info=True
+            )
+            raise e
+        except pvporcupine.PorcupineActivationThrottledError as e:
+            logger.error(
+                "[Porcupine] AccessKey '%s' has been throttled" % access_key,
+                stack_info=True,
+            )
+            raise e
+        except pvporcupine.PorcupineError as e:
+            logger.error("[Porcupine] 初始化 Porcupine 失败", stack_info=True)
+            raise e
+        except KeyboardInterrupt:
+            logger.info("Stopping ...")
+        finally:
+            porcupine and porcupine.delete()
+            recorder and recorder.delete()
 
     else:
         logger.info("使用 snowboy 进行离线唤醒")
